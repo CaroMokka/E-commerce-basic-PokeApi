b7068ae3667803ca5641b14a7ae16f5f2790ccc8
carolina <ci.arayagonzalez@gmail.com>
2022-04-15 21:21:27 -0400

    feat-Lastests

---
+++ src/front/js/components/Lastests.js
@@ -0,0 +1,20 @@
+import React from 'react';
+//styles
+import '../../styles/Lastests.css';
+//comaponents
+import { Card } from '../components/Card.js';
+
+export const Lastests = () => {
+    return (
+        <div className="lastests-container">
+            <div className="lastests-title">NEW ARRIVAL!</div>
+            <div className="lastests-body">
+                <Card />
+                <Card />
+                <Card />
+            </div>
+        </div>
+    )
+}
+
+
--- src/front/js/pages/Home.js
+++ src/front/js/pages/Home.js
@@ -1,18 +1,32 @@
 import React from 'react';
+//styles
+import '../../styles/Home.css';
 //Components
 import { Navbar } from '../components/Navbar.js';
 import { Search } from '../components/Search.js';
 import { Slider } from '../components/Slider.js';
+import { Lastests } from '../components/Lastests.js';
 import { Card }   from '../components/Card.js';
 
 
 export const Home = () => {
     return (
-        <div>
-            <Navbar />
-            <Search />
-            <Slider />
-            <Card />
+        <div className="home-container">
+            <div className="home-header">
+                <Navbar />
+            </div>
+            <div className="home-search">
+                <Search />
+            </div>
+            <div className="home-slider">
+                <Slider />
+            </div>
+            <div className="home-sponsors">
+                <Card />
+            </div>
+            <div className="home-lastests">
+                <Lastests />
+            </div>
         </div>
     );
 };
